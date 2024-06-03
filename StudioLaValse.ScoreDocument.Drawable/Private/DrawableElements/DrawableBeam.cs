948001fad336250b1c65e6491244e9abd7576d5d
Roel Westrik <roelwestrik@gmail.com>
2024-06-03 08:54:13 +0200

    fixed ruler bugfix

---
+++ StudioLaValse.ScoreDocument.Drawable/Private/DrawableElements/DrawableBeam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudioLaValse.ScoreDocument.Drawable.Private.DrawableElements;
+internal class DrawableBeam : DrawablePolygon
+{
+    public DrawableBeam(XY bottomLeft, XY bottomRight, double thickness, bool canvasUp, ColorARGB fill) : base(ToPoints(bottomLeft, bottomRight, thickness, canvasUp), fill)
+    {
+        
+    }
+
+    private static List<XY> ToPoints(XY bottomLeft, XY bottomRight, double thickness, bool canvasUp)
+    {
+        return canvasUp ?
+            [
+                bottomLeft, bottomRight, bottomRight - new XY(0, thickness), bottomLeft - new XY(0, thickness)
+            ] :
+            new List<XY>()
+            {
+                bottomLeft, bottomRight, bottomRight + new XY(0, thickness), bottomLeft + new XY(0, thickness)
+            };
+    }
+}
--- StudioLaValse.ScoreDocument.Drawable/Private/Models/VisualBeamBuilder.cs
+++ StudioLaValse.ScoreDocument.Drawable/Private/Models/VisualBeamBuilder.cs
@@ -25,14 +25,15 @@ namespace StudioLaValse.ScoreDocument.Drawable.Private.Models
                 return [];
             }
 
-            var crossGroup = stems.Any(s => s.VisuallyUp) && stems.Any(s => !s.VisuallyUp);
+            // Draw beams in the opposite direction of the first stem direction.
+            var drawBeamCanvasUp = !stems.First().VisuallyUp;
 
             var _beamThickness = beamThickness * scale;
             var _beamSpacing = beamSpacing * scale;
 
             return stems.Count() == 1
                 ? ([AsFlag(stems.First(), scale, color)])
-                : AsGroup(stems, beamDefinition, _beamSpacing, _beamThickness, crossGroup, color);
+                : AsGroup(stems, beamDefinition, _beamSpacing, _beamThickness, drawBeamCanvasUp, color);
         }
 
         public DrawableScoreGlyph AsFlag(VisualStem stem, double scale, ColorARGB color)
@@ -81,9 +82,9 @@ namespace StudioLaValse.ScoreDocument.Drawable.Private.Models
             return flag is null ? throw new UnreachableException("Invalid beaming information.") : flag;
         }
 
-        public IEnumerable<DrawableTrapezoid> AsGroup(IEnumerable<VisualStem> stems, Ruler beamDefinition, double beamSpacing, double beamThickness, bool crossGroup, ColorARGB color)
+        public IEnumerable<DrawableBeam> AsGroup(IEnumerable<VisualStem> stems, Ruler beamDefinition, double beamSpacing, double beamThickness, bool crossGroup, ColorARGB color)
         {
-            List<DrawableTrapezoid> beams = [];
+            List<DrawableBeam> beams = [];
 
             for (PowerOfTwo i = 8; i <= 128; i = i.Double())
             {
@@ -149,80 +150,75 @@ namespace StudioLaValse.ScoreDocument.Drawable.Private.Models
             return beams;
         }
 
-        public static DrawableTrapezoid BetweenTwoStems(VisualStem left, VisualStem right, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool crossGroup, ColorARGB color)
+        public static DrawableBeam BetweenTwoStems(VisualStem left, VisualStem right, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool drawBeamCanvasUp, ColorARGB color)
         {
             var stemUp = left.VisuallyUp;
+
+            // Always move the ruler agains the direction of the stem.
+            // If the stem goes up (from the note), move the ruler downwards.
+            // Starts at the tip, moves towards the note.
             var ruler = stemUp ?
-                //draw beams downwards
+                // Move the ruler downwards. Note the inverted y coordinates of the canvas. Y = 0 is top of canvas, etc.
                 beamDefinition.OffsetY(beamIndex * (beamSpacing + beamThickness)) :
-                //draw beams upwards
+                // Move the ruler upwards.
                 beamDefinition.OffsetY(beamIndex * ((beamSpacing + beamThickness) * -1));
 
-            var thickness = stemUp ?
-                beamThickness :
-                beamThickness * -1;
+            // Unless... When we draw betweeen two stems and it is unclear which direction to choose.
+            // Then choose the direction of the group as a whole.
+            // Note that the direction of the beam thickness never changes.
+            if (left.VisuallyUp != right.VisuallyUp)
+            {
+                ruler = drawBeamCanvasUp ?
+                    // Move the ruler upwards. Note the inverted y coordinates of the canvas. Y = 0 is top of canvas, etc.
+                    beamDefinition.OffsetY(beamIndex * (beamSpacing + beamThickness * -1)) :
+                    // Move the ruler downwards.
+                    beamDefinition.OffsetY(beamIndex * (beamSpacing + beamThickness));
+            }
 
             var startPoint = ruler.IntersectVerticalRay(left.End);
-
             var endPoint = ruler.IntersectVerticalRay(right.End);
 
-            return new DrawableTrapezoid(startPoint, endPoint, thickness, color);
+            return new DrawableBeam(startPoint, endPoint, beamThickness, drawBeamCanvasUp, color);
         }
 
-        public static DrawableTrapezoid AsHookEnd(VisualStem stem, double length, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool crossGroup, ColorARGB color)
+        public static DrawableBeam AsHookEnd(VisualStem stem, double length, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool drawBeamCanvasUp, ColorARGB color)
         {
             var stemUp = stem.VisuallyUp;
 
+            // Always move the ruler against the direction of the stem.
+            // Hooks are not affected by cross beams. Beams are always drawn in the direction of the group.
+            // The ruler is moved agains the direction of the stem.
             var ruler = stemUp ?
-                //draw beams downwards
+                // Move the ruler downwards. Note the inverted y coordinates of the canvas. Y = 0 is top of canvas, etc.
                 beamDefinition.OffsetY(beamIndex * (beamSpacing + beamThickness)) :
-                //draw beams upwards
+                // Move the ruler upwards.
                 beamDefinition.OffsetY(beamIndex * ((beamSpacing + beamThickness) * -1));
-
-            if (crossGroup)
-            {
-                ruler = stemUp ?
-                    ruler.OffsetY(beamThickness * -1) :
-                    ruler.OffsetY(beamThickness);
-            }
-
-            var thickness = stemUp ?
-                beamThickness :
-                beamThickness * -1;
 
             var endPoint = ruler.IntersectVerticalRay(stem.End);
 
             var startPoint = endPoint.Move(length * -1, ruler.Angle.ToRadians());
 
-            return new DrawableTrapezoid(startPoint, endPoint, thickness, color);
+            return new DrawableBeam(startPoint, endPoint, beamThickness, drawBeamCanvasUp, color);
         }
 
-        public static DrawableTrapezoid AsHookStart(VisualStem stem, double length, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool crossGroup, ColorARGB color)
+        public static DrawableBeam AsHookStart(VisualStem stem, double length, int beamIndex, Ruler beamDefinition, double beamSpacing, double beamThickness, bool drawBeamCanvasUp, ColorARGB color)
         {
             var stemUp = stem.VisuallyUp;
 
+            // always move the ruler against the direction of the stem.
+            // Hooks are not affected by cross beams. Beams are always drawn in the direction of the group.
+            // The ruler is moved agains the direction of the stem.
             var ruler = stemUp ?
-                //draw beams downwards
+                // Move the ruler downwards. Note the inverted y coordinates of the canvas. Y = 0 is top of canvas, etc.
                 beamDefinition.OffsetY(beamIndex * (beamSpacing + beamThickness)) :
-                //draw beams upwards
+                // Move the ruler upwards.
                 beamDefinition.OffsetY(beamIndex * ((beamSpacing + beamThickness) * -1));
-
-            if (crossGroup)
-            {
-                ruler = stemUp ?
-                    ruler.OffsetY(beamThickness * -1) :
-                    ruler.OffsetY(beamThickness);
-            }
-
-            var thickness = stemUp ?
-                beamThickness :
-                beamThickness * -1;
 
             var startPoint = ruler.IntersectVerticalRay(stem.End);
 
             var endPoint = startPoint.Move(length, ruler.Angle.ToRadians());
 
-            return new DrawableTrapezoid(startPoint, endPoint, thickness, color);
+            return new DrawableBeam(startPoint, endPoint, beamThickness, drawBeamCanvasUp, color);
         }
     }
 }
